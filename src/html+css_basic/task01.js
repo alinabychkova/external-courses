54b31107da526c9db6bc864f40002d8cb2f1e799
Alina Bychkova <alina.bychkova97@mail.ru>
2018-10-26 15:19:19 +0300

    task01.js

---
+++ src/html+css_basic/task01.js
@@ -0,0 +1,70 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Главная страница</title>
+    <meta name="keywords" content="HTML" />
+<style type="text/css">
+   .background { 
+    background: #02a325;
+}
+</style> 
+  </head>
+
+  <body>
+    <h1>Заголовок 1</h1>
+    <hr/>
+    <h2>Заголовок 2</h2>
+    <hr/>
+    <h3>Заголовок 3</h3>
+    <hr/>
+    <h4>Заголовок 4</h4>
+    <hr/>
+    <h5>Заголовок 5</h5>
+    <hr/>
+    <h6>Заголовок 6</h6>
+
+    <ol>
+     <li>Пункт1</li>
+     <li>Пункт2</li>
+     <li>Пункт3</li>
+    </ol>
+
+    <div class = "background">
+      Блочный элемент div
+    </div>
+
+    <table border="1">
+     <tr>
+       <td>Ячейка1</td>
+       <td>Ячейка2</td>
+       <td>Ячейка3</td>
+     </tr>
+     <tr>
+        <td>Ячейка1</td>
+        <td>Ячейка2</td>
+        <td>Ячейка3</td>
+     </tr>
+    </table>
+
+    <img src = "https://yandex.ru/images/search?pos=0&img_url=https%3A%2F%2Fcs9.pikabu.ru%2Fpost_img%2F2018%2F03%2F14%2F4%2Fog_og_1521000234285197728.jpg&text=%D0%BA%D0%BE%D1%82%D0%B8%D0%BA%D0%B8&rpt=simage" alt="какая-то картинка">
+
+    <a href = "http://pesni.tnt-online.ru/video.html">Ссылка на сайт</a>
+    
+    <pre>        Ты, меня любивший фальшью
+          Истины — и правдой лжи,
+           Ты, меня любивший — дальше
+             Некуда! — За рубежи!
+
+         Ты, меня любивший дольше
+          Времени. — Десницы взмах!
+           Ты меня не любишь больше:
+            Истина в пяти словах.
+    </pre>
+
+   <p>Абзац1</p><br>
+   <p>Абзац2</p><br>
+   <p>Абзац3</p><br>
+   <p>Абзац4</p><br>
+
+  </body>
+</html>
