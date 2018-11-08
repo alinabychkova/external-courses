ee892ff467f6feca68d719e8deafca5bfb520776
epam <epam@local.rsu.edu.ru>
2018-11-08 20:15:06 +0300

    task01.png

---
+++ src/ex4_js-objects-part2/task09.js
@@ -0,0 +1,12 @@
+var a ='там';
+var b = 'они были студентами';
+var temp = new Array();
+temp = b.split(' ');
+s1 = temp[0] + a
+s2 = temp[0].substring(1, 4);
+
+k1 = temp[1].substring(0, 1);
+k2 = temp[1].substring(1, 5);
+
+z1 = temp[2].substring(0, 1);
+z2 = temp[2].substring(1, 10);
